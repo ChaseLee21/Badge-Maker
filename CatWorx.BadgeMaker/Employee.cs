c0ae21f1335dccc65883147814497904dde53bff
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-25 16:13:17 -0700

    updating employee class to have id and photoUrl properties

--- CatWorx.BadgeMaker/Employee.cs
+++ CatWorx.BadgeMaker/Employee.cs
@@ -5,11 +5,13 @@ namespace CatWorx.BadgeMaker
     public string FirstName;
     public string LastName;
     public int Id;
-    // public string PhotoUrl;
-    public Employee(string firstName, string lastName)
+    public string PhotoUrl;
+    public Employee(string firstName, string lastName, int id, string photoUrl)
     {
         FirstName = firstName;
         LastName = lastName;
+        Id = id;
+        PhotoUrl = photoUrl;
     }
     public string GetFullName()
     {
