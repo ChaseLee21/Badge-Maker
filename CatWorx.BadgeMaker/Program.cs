358cdb6bc6e33cae24748cd8f727b5b8fc0d9f98
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-25 15:19:42 -0700

    iterrating a list of employees

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -5,6 +5,11 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
+        List<String> employees = new List<String>() { "Max", "Sally", "John" };
+        for (int i = 0; i < employees.Count; i++) 
+        {
+            Console.WriteLine(employees[i]);
+        }
       Console.WriteLine("Hello, World!");
     }
   }
