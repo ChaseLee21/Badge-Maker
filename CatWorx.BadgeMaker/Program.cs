6ea7665ebd1e186d0ae7b75721a4147d2ebc3cfc
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-25 15:24:54 -0700

    assign a console input to a variable and add it to an array

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -6,11 +6,17 @@ namespace CatWorx.BadgeMaker
     static void Main(string[] args)
     {
         List<String> employees = new List<String>() { "Max", "Sally", "John" };
+        employees.Add("Billy");
+        employees.Add("Barbra");
+        Console.WriteLine("Please enter an employee name: ");
+        string input = Console.ReadLine() ?? "";
+        employees.Add(input);
+        Console.WriteLine("------------");
+        Console.WriteLine("Employees: ");
         for (int i = 0; i < employees.Count; i++) 
         {
             Console.WriteLine(employees[i]);
         }
-      Console.WriteLine("Hello, World!");
     }
   }
 }
